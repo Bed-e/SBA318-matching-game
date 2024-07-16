9b51069257deca1256c311322c9d2f2268ab67ca
Bed-e <ben17wilson@gmail.com>
2024-07-16 16:59:55 -0500

    most requirements met. app functions. little error handling middleware

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// index.js
 const express = require("express");
 const session = require("express-session");
 const bodyParser = require("body-parser");
--- users.js
+++ users.js
@@ -1,6 +1,6 @@
 const users = [
   // Example user
-  { username: "test", password: "test", highScore: 0 },
+  { username: "test", password: "test", highScore: 0, comments: [] },
 ];
 
 module.exports = users;
